98ae39303ce1f3f6fe3ac968d3dbc36950449143
yhkuo <yskuo@ucdavis.edu>
2023-01-15 15:40:46 -0800

    fixed bugs

--- Assignment/Vue-Flask-Template/dashboard/src/components/Notes.vue
+++ Assignment/Vue-Flask-Template/dashboard/src/components/Notes.vue
@@ -1,17 +1,37 @@
 <script lang="ts">
 import { useExampleStore } from '../stores/exampleStore';
-import { watch } from 'vue';
+import { mapState } from 'pinia'
+import { watch, ref } from 'vue';
+
+//NOTE: you can disregard any composition API syntax if you stick with optionAPI (which I recommmend)
+//If you want to understad how the store works here, make sure you load the <ExampleWithInteractionns> component to check the expected channges
 
 export default {
-  setup() {
+  /*setup() {
     const store = useExampleStore()
     // This is the state read from store
     // This is showing how the store update from one component will also be reflected in other components
     watch(store, (state) => { 
       console.log(state.size)
     })
+    // Setting up the local state
+    const count = ref(0)
     return {
-      count: 0 // This is the local state of this component
+      count // This is the local state of this component
+    }
+  },*/
+  //This is the equivalent code (data, computed, and watch) to the setup() above, but in options API
+  data() {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    ...mapState(useExampleStore, ['size']) // Traditional way to map the store state to the local state
+  },
+  watch:{ // This is only effective when you load with <ExampleWithInteractionns> component
+    size(){
+      console.log(this.size)
     }
   },
   props: {
