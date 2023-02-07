da79f427a71686129e1313594e7bbd5cfd5b2697
Qifan Rem <prisonbusterq234@gmail.com>
2023-02-07 02:08:08 -0800

    Assignment

--- Assignment/Vue-Flask-Template/server/controller.py
+++ Assignment/Vue-Flask-Template/server/controller.py
@@ -2,9 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.datasets import load_wine
 from resources.hd_processing_template import perform_PCA, perform_TSNE
-#from resources.network_process_template import contsruct_networkx
-#from resources.text_processing_template import preprocess
-#from resources.time_processing_template import prepare_time_template_data, apply_arima, apply_sarima
 
 def processExample(method: str = 'PCA') -> tuple[list[dict], list[int]]:
     data: dict = load_wine()
